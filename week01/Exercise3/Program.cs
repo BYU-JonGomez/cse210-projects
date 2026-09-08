abe80d900cf6eaaa03431ff1b24c4e00b5a57086
BYU-JonGomez <j.gomez.alegria@example.com>
2026-09-08 14:21:58 -0300

    Exercise 3 finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        int counter = 0;
+
+        while (guess != magicNumber)
+        {
+            counter++;
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber == guess)
+            {
+                Console.WriteLine("You guessed it! It took you " + counter + " tries.");
+                Console.WriteLine("Do you want to play again? (y/n)");
+                string playAgain = Console.ReadLine();
+                if (playAgain.ToLower() == "y")
+                {
+                    magicNumber = randomGenerator.Next(1, 101);
+                    guess = -1;
+                    counter = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    break;
+                }
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher. ");
+            }
+            else
+            {
+                Console.WriteLine("Lower. ");
+            }
+        }
     }
 }
