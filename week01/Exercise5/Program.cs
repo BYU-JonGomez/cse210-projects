c7fb749d0665a015af138df3fe327a884ce20205
BYU-JonGomez <j.gomez.alegria@example.com>
2026-09-12 22:59:25 -0300

    Exercise 5 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string name = PromptUserName();
+        int squaredNumber = FavoriteNumber();
+
+        Console.WriteLine($"{name}, the square of your favorite number is {squaredNumber}.");
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int FavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        int squaredNumber = favoriteNumber * favoriteNumber;
+        return squaredNumber;
     }
 }
